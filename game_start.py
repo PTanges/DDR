d9ae3bf2bfb7173470bb8971c4dd5e8e2e54ff02
PTang <pattontanges@gmail.com>
2024-04-01 17:15:18 -0700

    Initialization

---
+++ game_start.py
@@ -0,0 +1,14 @@
+'''
+Name: Patton Tang
+Course: CPSC 386 w Professor McCarthy
+Institution: CSU Fullerton
+
+Task: Dance Dance Revolution / Guitar Hero / Friday Night Funkin
+- Must include four arrow keys AND simultaneous keypad inputs
+'''
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
