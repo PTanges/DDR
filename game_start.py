98eea30c377ea9b70e246660ec380379e3242eaf
PTang <pattontanges@gmail.com>
2024-04-03 14:08:16 -0700

    RNG Note generation, fixed image loading (note sprites), and proper note termination along tracks. Visuals implemented (& performance adjustments)

--- game_start.py
+++ game_start.py
@@ -15,6 +15,8 @@ from game_config import Settings
 from game_config import Config
 from keybinds import keyboard_commands
 from notes import Notes
+from notes import Note as Note_Class
+from notes import Track
 from audio import Audio
 from pygame import mixer
 from scoreboard import Scoreboard
@@ -27,9 +29,11 @@ class GM:
         self.settings = Settings()
         self.config = Config()
         self.screen = pygame.display.set_mode(self.settings.screen_dimensions)
+        self.background = self.screen.copy()
         self.clock = pg.time.Clock()
         self.audio = Audio()
         self.game_powerbox_switch = True
+        self.sprites = pg.sprite.Group()
         active_state = ""
 
     def game_loop(self):
@@ -62,7 +66,9 @@ class GM:
         # end while
 
     def pregame_initialization(self):
-        self.screen.fill("blue")
+        # Move to Play once mult tracks are added
+        self.create_tracks()
+        self.screen.fill("black")
         self.update_display()
         return
 
@@ -70,7 +76,8 @@ class GM:
         # Automatically going to choose Rain Garden
         print("Playing Rain Garden.mp3...")
 
-        # Scoreboard keeps a list of song_data's, use for displaying the tracks on selection screen
+        # Scoreboard keeps a list of song_data's,
+        # use for displaying the tracks on selection screen
         # SONG_NAME = ...
         # self.credits = ...
         # self.settings = ...
@@ -89,21 +96,39 @@ class GM:
 
     def play_track_screen(self, key_manager):
         notes = Notes(game=self)
-        # Draw Tracks + Text
-
         self.audio.play_music(self.audio.track01)
+        notes.randomly_generate_note()
+        ticks = 0
+        prev_ticks = 0
         while mixer.get_busy() == False:
-            # Draw Notes
-
+            if ticks < prev_ticks:
+                notes.randomly_generate_note()
+            prev_ticks = ticks
 
             for event in pygame.event.get():
                 if event.type in key_manager.keyactions:
+                    # Play a 'donk' sfx (SFX by Toby Fox would be appropriate)
+                    # with a rand.pitch +/-0.05, with "Perfect" being the highest...
+
                     # Peek into respective column (note sprite group) for up/down/etc
-                    # Measure distance from track_bottom to closest note (loop through respective sprite group)
-                    # - if one sprite group, add value for L R Up D and then if for if col1-U and col2-L, etc
+                    # Measure distance from track_bottom to closest note
+                    # (loop through respective sprite group)
+                    # - if one sprite group, add value for L R Up D
+                    # and then if for if col1-U and col2-L, etc
                     pass
+                elif event.type == pg.QUIT:
+                    action = "EXIT"
+                    return action
             # move notes down
-            # update display
+            notes.note_group.update()
+
+            self.screen.fill("black") # fill required each time or once?
+            self.sprites.add(notes.note_group)
+            self.sprites.draw(self.screen)
+            self.update_display()
+            
+            delta = self.clock.tick(self.settings.FPS)
+            ticks = (ticks + delta) % 500
         # end while
 
         action = "RESULTS"
@@ -111,10 +136,40 @@ class GM:
 
 
     def update_display(self):
-        pg.display.flip()
-        self.clock.tick(self.settings.FPS)
-        # delta_time = self.clock.tick(self.settings.FPS) / 1000 for framerate indepdendent physics
+        pg.display.update()
 
+    def create_tracks(self):
+        track_size = 40
+        track_x_distance = self.screen.get_width() // 9
+        track_length = self.screen.get_width() // track_size
+
+        x = 1
+        for _ in range(4):
+            track_y = self.screen.get_height() - track_size
+            track_x = (track_x_distance * x) + (track_size / 2)
+            self.sprites.add(Track(
+                size=track_size,
+                x=track_x,
+                y=track_y,
+                isBottom=True))
+            x += 2
+        
+        section: pg.Surface
+        x = 1
+        y = 1 # Count including the track bottom
+        for _ in range(4):
+            for _ in range(track_length):
+                track_y = self.screen.get_height() - (track_size * y)
+                track_x = (track_x_distance * x) + (track_size / 2)
+                self.sprites.add(Track(
+                    size=track_size,
+                    x=track_x,
+                    y=track_y,
+                    isBottom=False))
+                y += 1
+            x += 2
+            y = 1
+    
     class _game_states:
         def __init__(self):
             self.game_start = "START"
@@ -123,11 +178,13 @@ class GM:
             self.results = "RESULTS"
             self.credits = "CREDITS"
             self.exit = "EXIT"
-            self.states = [self.game_start, self.selection, self.play, self.results, self.credits, self.exit]
+            self.states = [self.game_start, self.selection, self.play,
+                            self.results, self.credits, self.exit]
 
         def identify_state_transition(self, active_state, state_machine_input):
             # Note:
-            # States should maintain self state transitions (given an input), may need to alter the structure above
+            # States should maintain self state transitions (given an input),
+            # may need to alter the structure above
             # ie state NAMES become global and define per-state-transition actions
             next_state = active_state
             if active_state == self.game_start:
--- notes.py
+++ notes.py
@@ -13,7 +13,7 @@ class Notes:
         self.game = game
         self.initialize_image_files()
 
-        self.screen = game.screen
+        self.screen: pg.Surface = game.screen # encforcement
         self.settings = game.settings
         self.config = game.config
         self.scoreboard = game.scoreboard
@@ -21,25 +21,24 @@ class Notes:
 
         self.note_group = pg.sprite.Group()
 
-    def initialize_image_files(self):
 
+    def initialize_image_files(self):
         track_arrow_files = []
         NIA = ["Up", "Left", "Right", "Down"]
-        self.note_images = [pg.transform.scale(pg.image.load(f'Images/Static/BW_{x}_Arrow.png'), (40, 40)) for
-                                   x in NIA]
-        self.track_arrow_images = [pg.transform.scale(pg.image.load(f'Images/Static/Frame_{x}_Arrow.png'), (40, 40)) for
-                                   x in NIA]
-        self.track_images.append(pg.transform.scale(pg.image.load(f'Images/Static/Track_Path.png'), (40,40)))
-        self.track_images.append(pg.transform.scale(pg.image.load(f'Images/Static/Track_Bottom.png'), (40,40)))
+        self.note_images = [pg.transform.scale(
+            pg.image.load(f'Images/Static/BW_{x}_Arrow.png'), (40, 40)) for x in NIA]
+        self.track_arrow_images = [pg.transform.scale(
+            pg.image.load(f'Images/Static/Frame_{x}_Arrow.png'), (40, 40)) for x in NIA]
+        # enforce the -8 syntax rules (char limit per line)
 
-
-
+    ''' # referenced from Aliens in Galga
     def add_note(self, note_column, x, y, note_type):
         note = Note(self.game)
         note.y = y
         note.rect.x, note.rect.y = x, y
         # use note_type to dictate the x and then into the correct note column
         self.note_group.add(note)
+    '''
 
     def reset(self):
         self.empty(self.note_group)
@@ -48,18 +47,28 @@ class Notes:
         sprite_group.empty()
 
     def randomly_generate_note(self):
-        pass
+        note = Note(self.game)
+        position = randint(1, 4) # [1,4] inclusive
+        note.direction = note.note_types[position-1]
+        note.y = 0
+        note.x = (position * 2) - 1
+        note.image = self.note_images[position - 1]
+        note.rect.x = note.x * (self.screen.get_width() // 9) + (40 / 2)
+        note.rect.y = note.y
+        self.note_group.add(note)
 
+    '''
     def check_baseline(self):
         pass
-        '''
+        
         for note in self.note_group():
             if note.y > track_bottom.y:
                 note.isCrossLine = True
-        '''
+    '''
 
 class Note(Sprite):
     note_images = []
+    note_types = ["UP", "LEFT", "RIGHT", "DOWN"]
     def __init__(self, game):
         super().__init__()
         self.game = game
@@ -69,36 +78,39 @@ class Note(Sprite):
         self.settings = game.settings
         self.config = game.config
         self.scoreboard = game.scoreboard
-        self.gravity = Vector(0, 5)
+        self.gravity = Vector(0, 2.5)
+        self.direction = ""
 
         self.image = self.note_images[0]
         self.rect = self.image.get_rect()
         self.rect.x = self.rect.width
         self.rect.y = self.rect.height
+        self.x = float(self.rect.x)
         self.y = float(self.rect.y)
         self.isCrossLine = False # ignored by key check once < height of baseline
 
     def initialize_image_files(self):
         NIA = ["Up", "Left", "Right", "Down"]
 
-        note_image_files = []
-        note_image_files.append(f'Images/Static/BW_{x}_Arrow.png' for x in NIA)
-        self.note_images = [pg.transform.scale(pg.image.load(x), (40, 40)) for x in note_image_files]
+        note_image_files = [f'Images/Static/BW_{x}_Arrow.png' for x in NIA]
+        self.note_images = [pg.transform.scale(pg.image.load(file),(40, 40))
+                            for file in note_image_files]
 
-    def update(self, gravity):
-        self.y += gravity
+    def update(self):
+        self.y += self.gravity.y
         self.rect.y = self.y
+        self.check_floor()
         if self.isCrossLine:
+            print(f'{self.direction} arrow terminated!')
             self.kill()
-        self.draw()
 
     def draw(self):
         # self.image = self.timer.current_image() once you want to make a gif of the arrow
         self.screen.blit(self.image, self.rect)
 
     def check_floor(self):
-        # if sprite collides with floor (edge of the screen), kill the sprite
-        pass
+        if self.rect.y > self.screen.get_height():
+            self.isCrossLine = True
     '''
     Implementation Idea:
     To assign beats to a beatmap (instead of manual), play the song and map the beats (in real time) to key presses
@@ -106,3 +118,21 @@ class Note(Sprite):
     - Read from File
     - Enter a debug menu/action when playing a song
     '''
+
+class Track(Sprite):
+    def __init__(self, size, x, y, isBottom):
+        super().__init__()
+        self.image: pg.Surface
+
+        if isBottom:
+            self.image = pg.transform.scale(
+                pg.image.load(f'../../Downloads/DDR/DDR/Images/Static/Track_Bottom.png'),
+                (size, size))
+        else:
+            self.image = pg.transform.scale(
+                pg.image.load(f'../../Downloads/DDR/DDR/Images/Static/Track_Path.png'),
+                (size, size))
+        
+        self.rect = self.image.get_rect()
+        self.rect.top = y
+        self.rect.left = x
